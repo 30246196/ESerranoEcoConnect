fe3eebe20930f896559bae2637421e580bac7553
30246196 <edithserra1@gmail.com>
2026-04-30 10:33:27 +0100

    Add project files.

---
+++ ESerranoEcoConnect/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ESerranoEcoConnect.MvcApplication" Language="C#" %>
---
+++ ESerranoEcoConnect/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ESerranoEcoConnect.Startup))]
+namespace ESerranoEcoConnect
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
